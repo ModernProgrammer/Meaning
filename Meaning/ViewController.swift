26db3b61d40f39226ee9914e493c148c5b59acc5
ModernProgrammer <diegobust4545@icloud.com>
2022-12-16 17:59:14 -0500

    first commit

---
+++ Meaning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meaning
+//
+//  Created by Diego Bustamante on 12/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
